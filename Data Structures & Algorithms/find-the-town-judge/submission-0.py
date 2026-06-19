a8c36e0582d2cc8e474ff076b9e6e51f5c5a851b
HemanthReddyGaddam <114926529+HemanthReddyGaddam@users.noreply.github.com>
2026-06-19 07:55:01 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        mp={}
+        ans=-1
+        count=0
+        for i,j in trust:
+            if j not in mp:
+                mp[j]=[]
+            mp[j].append(i)
+            if len(mp[j])==n-1:
+                count+=1
+                ans=j
+        if count!=1:
+            return -1
+        for i,j in trust:
+            if i==ans:
+                return -1
+        return ans
+
+
+        
